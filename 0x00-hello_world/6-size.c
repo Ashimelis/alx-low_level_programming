b39f7bdc062c26f8fff6de129d003b2d8d088a12
Ashimelis <shimelisget2@gmail.com>
2022-10-14 10:01:33 -0700

    Task6C Updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main - Entery
- * Return: Always 0
+ * main - Entery point
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("size of char: %i byte(s)\n", sizeof(char));
-	printf("Size of int: %i byte(s)\n", sizeof(int));
-	printf("Size of long int: %i byte(s)\n", sizeof(long int));
-	printf("size of long long int: %i byte(s)\n", sizeof(long long int));
-	printf("size of float: %i byte(s)\n", sizeof(float));
+	printf("size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
