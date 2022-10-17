7f8d81b6cdd7cdefd31b4227643228e1442bdeac
Ashimelis <shimelisget2@gmail.com>
2022-10-16 18:14:44 -0700

    Task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,10 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-		putchart(ch);
+		putchar(ch);
 	for (ch = 'A'; ch <= 'Z'; ch++)
-		putchart(ch);
-	putchart('\n');
+		putchar(ch);
+	putchar('\n');
 
 	return (0);
 }
